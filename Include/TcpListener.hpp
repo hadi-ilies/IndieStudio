fbabacdb606921e3abed6592a12a324cb83f98de
Corentin GAUTIER <corentin.gautier@epitech.eu>
2019-05-17 12:04:11 +0300

    proto socket

---
+++ Include/TcpListener.hpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2019
+** for_norme
+** File description:
+** TcpListener.hpp
+*/
+
+#ifndef TCPLISTENER_HPP
+#define TCPLISTENER_HPP
+
+#include <string>
+//#include "Socket.hpp" // ?
+
+using namespace std;
+
+class TcpListener// : public Sokect // ?
+{
+public:
+    TcpListener();
+    ~TcpListener();
+    const uint &getLocalPort() const;
+    bool listen(const uint &port);
+    //void close(); // ?
+    bool accept(TcpSocket &socket);
+
+private:
+    std::string remoteAddress;
+    uint remotePort;
+};
+
+#endif
---
+++ Include/TcpSocket.hpp
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2019
+** for_norme
+** File description:
+** TcpSocket.hpp
+*/
+
+#ifndef TCPSOCKET_HPP
+#define TCPSOCKET_HPP
+
+#include <string>
+
+using namespace std;
+
+class TcpSocket
+{
+public:
+    TcpSocket();
+    ~TcpSocket();
+    const uint &getLocalPort() const;
+    const std::string &getRemoteAddress() const;
+    const uint &getRemotePort() const;
+    bool connect(const std::string &address, const uint &port);
+    void disconnect();
+    //send // TODO
+    //receive // TODO
+
+private:
+    std::string remoteAddress;
+    uint remotePort;
+};
+
+#endif
