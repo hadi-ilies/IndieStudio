2288ffa2cf07a63686628dadcec65e769ab8fc8a
Parallels <hadi-ilies.bereksi-reguig@epitech.eu>
2019-05-23 14:49:38 +0200

    Client Part 2

--- Src/Client.cpp
+++ Src/Client.cpp
@@ -17,59 +17,52 @@ using namespace std;
 using namespace irr;
 using namespace sf;
 
-/*
-static void game(Window &window)
+static void game(Window &window, Receiver &receiver, Sender &sender)
 {
     //World world(window, worldFileName); // TODO create with socket
-    Window window("Bomberman", dimension2d<u32>(1920 / 2, 1080 / 2), false);
     //Window window("Bomberman", dimension2d<u32>(1920, 1080), true);
     World world(window, "TODO");
     Player player(window, "Resources/Entity/Bomberman", "Bob", world, vector3du(1, 1, 1));
     bool spacePress = false; // tmp
-
-    //world.debugAff();
-    while (window.isOpen()) {
-        // TODO
-        if (window.isKeyPressed(KEY_ESCAPE))
-            window.close();
-        else if (window.isKeyPressed(KEY_KEY_Q) || window.isKeyPressed(KEY_KEY_A))
-            player.move(vector2di(-1, 0));
-        else if (window.isKeyPressed(KEY_KEY_D))
-            player.move(vector2di(1, 0));
-        else if (window.isKeyPressed(KEY_KEY_Z) || window.isKeyPressed(KEY_KEY_W))
-            player.move(vector2di(0, 1));
-        else if (window.isKeyPressed(KEY_KEY_S))
-            player.move(vector2di(0, -1));
-        else if (window.isKeyPressed(KEY_SPACE)) {
-            if (!spacePress)
-                player.putBomb();
-            spacePress = true;
-        }
-        else {
-            player.move(vector2di(0, 0));
-            spacePress = false;
-        }
-        world.update();
-        player.update();
-        window.display(video::SColor(255, 113, 113, 233));
-    }
-}*/
-
-void client(const IpAddress &ip, const ushort &port)
-{
-    TcpSocket client;
-    if (client.connect(ip, port) != Socket::Done)
-        throw Error("an Error has been detected in Connect function");
-    Window window("Bomberman", dimension2d<u32>(1920, 1080), false);
-    Receiver receiver(client);
 
     while (window.isOpen()) {
         if (receiver.receive()) {
             if (receiver.type == Message) {
                 cout << "RECEIVED :" << receiver.message << endl;
             }
-        }
+
+            //tmp2
+            if (window.isKeyPressed(KEY_ESCAPE))
+                window.close();
+            else if (window.isKeyPressed(KEY_KEY_Q) || window.isKeyPressed(KEY_KEY_A))
+                sender.sendPlayerMove(vector2di(-1, 0));
+            else if (window.isKeyPressed(KEY_KEY_D))
+                sender.sendPlayerMove(vector2di(1, 0));
+            else if (window.isKeyPressed(KEY_KEY_Z) || window.isKeyPressed(KEY_KEY_W))
+                sender.sendPlayerMove(vector2di(0, 1));
+            else if (window.isKeyPressed(KEY_KEY_S))
+                sender.sendPlayerMove(vector2di(0, -1));
+            else if (window.isKeyPressed(KEY_SPACE)) {
+                if (!spacePress)
+                    sender.sendPlayerPutBomb;
+            else
+                sender.sendPlayerMove(vector2di(0, 0));
+            world.update();
+            player.update();
+            window.display(video::SColor(255, 113, 113, 233));
+            }
     }
+}
+
+void client(const IpAddress &ip, const ushort &port)
+{
+    TcpSocket client;
+    if (client.connect(ip, port) != Socket::Done)
+        throw Error("an Error has been detected in Connect function");
+    Receiver receiver(client);
+    Sender sender(client);
+    Window window("Bomberman", dimension2d<u32>(1920 / 2, 1080 / 2), false);
+    game(window, receiver, sender);
     //structure enum type data
     // union
 }
