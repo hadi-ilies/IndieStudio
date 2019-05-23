17c9158652883a3e1458296b930cdaa7ee1b51d2
Parallels <hadi-ilies.bereksi-reguig@epitech.eu>
2019-05-23 13:04:19 +0200

    Throw Error on connect

--- Src/Client.cpp
+++ Src/Client.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream> // ?
 #include "Window.hpp"
+#include "Error.hpp"
 #include "World.hpp"
 #include "Entity/Player.hpp"
 #include "Sender.hpp"
@@ -24,16 +25,15 @@ void client(const IpAddress &ip, const ushort &port)
 {
     cerr << ip << "  " << port << endl;
     //Window window("Bomberman", dimension2d<u32>(1920, 1080), true);
-    IpAddress ipAddr(ip);
     TcpSocket client;
 
 
-    cerr << client.connect(ipAddr, port) << endl;
-    //if (client.connect(ipAddr, port) != Socket::Done)
-    //    cerr << lient.connect(ipAddr, port) << endl;
+    if (client.connect(ip, port) != Socket::Done)
+        throw Error("an Error has been detected in Connect function");
     Packet packet;
     int type;
     client.receive(packet);
+
     packet >> type;
     cout << type << endl;
     if (type == Message) {
