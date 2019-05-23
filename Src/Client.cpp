021e5357364bd14d9426ec560f1468751a93cc09
Parallels <hadi-ilies.bereksi-reguig@epitech.eu>
2019-05-23 16:01:34 +0200

    CLient part 4

--- Src/Client.cpp
+++ Src/Client.cpp
@@ -6,66 +6,71 @@
 */
 
 #include <iostream> // ?
+#include <thread>
 #include "Window.hpp"
 #include "Error.hpp"
 #include "World.hpp"
 #include "Entity/Player.hpp"
-#include "Sender.hpp"
-#include "Receiver.hpp"
+#include "FormattedSocket.hpp"
 
 using namespace std;
 using namespace irr;
 using namespace sf;
 
-static void game(Window &window, Receiver &receiver, Sender &sender)
+void serverLoop(FormattedSocket *client, Window *window, World *world, Player *player)
 {
-    //World world(window, worldFileName); // TODO create with socket
-    //Window window("Bomberman", dimension2d<u32>(1920, 1080), true);
-    World world(window, "TODO");
-    Player player(window, "Resources/Entity/Bomberman", "Bob", world, vector3du(1, 1, 1));
-
-    while (window.isOpen()) {
-        if (receiver.receive()) {
-            cerr << "receiver.type : " << receiver.type << endl; // tmp
-            if (receiver.type == StartTurn) {
+    while (window->isOpen()) {
+        if (client->receive()) {
+            cerr << "receiver->type : " << client->type << endl; // tmp
+            if (client->type == StartTurn) {
                 //tmp2
-                if (window.isKeyPressed(KEY_ESCAPE))
-                    window.close();
-                else if (window.isKeyPressed(KEY_KEY_Q) || window.isKeyPressed(KEY_KEY_A))
-                    sender.sendPlayerMove(vector2di(-1, 0));
-                else if (window.isKeyPressed(KEY_KEY_D))
-                    sender.sendPlayerMove(vector2di(1, 0));
-                else if (window.isKeyPressed(KEY_KEY_Z) || window.isKeyPressed(KEY_KEY_W))
-                    sender.sendPlayerMove(vector2di(0, 1));
-                else if (window.isKeyPressed(KEY_KEY_S))
-                    sender.sendPlayerMove(vector2di(0, -1));
-                else if (window.isKeyPressed(KEY_SPACE))
-                    sender.sendPlayerPutBomb();
+                if (window->isKeyPressed(KEY_ESCAPE))
+                    window->close();
+                else if (window->isKeyPressed(KEY_KEY_Q) || window->isKeyPressed(KEY_KEY_A))
+                    client->sendPlayerMove(vector2di(-1, 0));
+                else if (window->isKeyPressed(KEY_KEY_D))
+                    client->sendPlayerMove(vector2di(1, 0));
+                else if (window->isKeyPressed(KEY_KEY_Z) || window->isKeyPressed(KEY_KEY_W))
+                    client->sendPlayerMove(vector2di(0, 1));
+                else if (window->isKeyPressed(KEY_KEY_S))
+                    client->sendPlayerMove(vector2di(0, -1));
+                else if (window->isKeyPressed(KEY_SPACE))
+                    client->sendPlayerPutBomb();
                 else
-                    sender.sendPlayerMove(vector2di(0, 0));
-                if (receiver.receive()) {
-                    if (receiver.type == PlayerMove)
-                        player.move(receiver.dir);
-                    else if (receiver.type == PlayerPutBomb)
-                        player.putBomb();
+                    client->sendPlayerMove(vector2di(0, 0));
+                if (client->receive()) {
+                    if (client->type == PlayerMove)
+                        player->move(client->dir);
+                    else if (client->type == PlayerPutBomb)
+                        player->putBomb();
                 }
-                world.update();
-                player.update();
-                window.display(video::SColor(255, 113, 113, 233));
+                world->update();
+                player->update();
+                window->display(video::SColor(255, 113, 113, 233));
             }
         }
     }
 }
 
+static void game(Window &window, FormattedSocket &client)
+{
+    //World world(window, worldFileName); // TODO create with socket
+    //Window window("Bomberman", dimension2d<u32>(1920, 1080), true);
+    World world(window, "TODO");
+    Player player(window, "Resources/Entity/Bomberman", "Bob", world, vector3du(1, 1, 1));
+    std::thread loop(serverLoop, &client, &window, &world, &player);
+
+    loop.join();
+    //
+}
+
 void client(const IpAddress &ip, const ushort &port)
 {
-    TcpSocket client;
-    if (client.connect(ip, port) != Socket::Done)
+    FormattedSocket client;
+    if (!client.connect(ip, port))
         throw Error("an Error has been detected in Connect function");
-    Receiver receiver(client);
-    Sender sender(client);
     Window window("Bomberman", dimension2d<u32>(1920 / 2, 1080 / 2), false);
-    game(window, receiver, sender);
+    game(window, client);
     //structure enum type data
     // union
 }
