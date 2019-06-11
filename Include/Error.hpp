b421df72ee146576175cf21573039fb7e5f0d541
CamilleP <camillepolice@icloud.com>
2019-06-11 16:15:45 +0200

    Merge remote-tracking branch 'origin/master'

--- Include/Error.hpp
+++ Include/Error.hpp
@@ -1,7 +1,3 @@
-#include <utility>
-
-#include <utility>
-
 /*
 ** EPITECH PROJECT, 2019
 ** Bomberman
@@ -14,35 +10,33 @@
 
 #include <string>
 #include <exception>
-#include <utility>
 
-#include <string>
-#include <sstream>
+#define ERROR(str) Error(str, __FILE__, __FUNCTION__, __LINE__)
 
-class Error : public std::exception {
+class Error : public std::exception { // todo move in cpp ??
 public:
-    explicit Error(std::string what = "unknown", std::string file = "unknown", std::string function = "unknown", int line = 0) : _what(
-            std::move(what)), _file(std::move(file)), _function(std::move(function)), _line(line) {};
+    explicit Error(std::string what = "unknown", std::string file = "unknown",
+                   std::string function = "unknown", size_t line = 0) : _what(
+            std::move(what)), _file(std::move(file)), _function(
+            std::move(function)), _line(line) {};
     ~Error() = default;
 
 public:
     const char *what() const noexcept override {
         return this->_what.c_str();
     }
-    const char *where() {
-        std::stringstream ss;
 
-        ss << this->_file << " : " << this->_function << " : " << this->_line;
-        this->_where = ss.str();
-        return this->_where.c_str();
+    const char *where() const {
+        static std::string where = this->_file + " : " + this->_function
+                                   + " : " + to_string(this->_line);
+        return where.c_str();
     }
 
 protected:
-    std::string _what;
-    std::string _where;
-    std::string _file;
-    std::string _function;
-    int _line;
+    const std::string _what;
+    const std::string _file;
+    const std::string _function;
+    const size_t _line;
 };
 
 #endif /* !ERROR_HPP */
